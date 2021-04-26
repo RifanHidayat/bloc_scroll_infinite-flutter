0df40706569f6241f2c661be128d25d0d273b4e2
RezhaArenzha <rezha.arenzha@gmail.com>
2021-04-26 11:52:58 +0700

    final

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:bloc_infinite/bloc/post_bloc.dart';
+import 'package:bloc_infinite/ui/post_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,14 +10,26 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  ScrollController controller=ScrollController();
+  PostBloc bloc;
+
+  void onScroll(){
+    double maxcScroll=controller.position.maxScrollExtent;
+    double currentScroll =controller.position.pixels;
+    if (currentScroll ==  maxcScroll) bloc.add(PostEvent());
+
+  }
   @override
   Widget build(BuildContext context) {
+    bloc  =BlocProvider.of<PostBloc>(context);
+    controller.addListener(() {onScroll()});
     return Scaffold(
       appBar: AppBar(
         title: Text("Ifnite  list witch bloc"),
       ),
       body: Container(
-        margin: EdgeInsets.all(20),
+        margin: EdgeInsets.only(left: 20,right: 20),
         child: BlocBuilder<PostBloc, PostState>(
           builder: (context, state) {
             if (state is PostUninitialized) {
@@ -29,8 +43,13 @@ class _MainPageState extends State<MainPage> {
             } else {
               PostLoaded postLoaded = state as PostLoaded;
               return ListView.builder(
-                  itemBuilder: (context, index) =>
-                      Text(postLoaded.posts[index].title));
+                controller: controller,
+                itemCount: (postLoaded.hasReachedMax)?postLoaded.posts.length:postLoaded.posts.length+1,
+                  itemBuilder: (context, index) =>(index < postLoaded.posts.length) ?
+                  PostItem(post: postLoaded.posts[index],):Container(
+                    child: Center(child: SizedBox(height: 30,width: 30 ,child: CircularProgressIndicator(),)),
+                  )
+              );
             }
           },
         ),
